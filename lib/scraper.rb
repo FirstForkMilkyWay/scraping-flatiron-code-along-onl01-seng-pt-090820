9ff8c761cb9bb55b23df23e17ca5a42a0b8b6442
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-11 14:29:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,6 +19,13 @@ class Scraper
   
   def get_page
       doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+      
+      
+      
+      
+      
+      
+      
   end
   
   def get_courses 
